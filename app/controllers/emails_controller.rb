30df8920ea99cb8633b89dccf65f818e6566f2bb
CocoThePimp <jonathan.lutz@gmx.fr>
2019-02-28 17:21:35 +0100

    css ok

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,14 +1,19 @@
 class EmailsController < ApplicationController
 
-def index
- @emails = Email.all
- respond_to do |format|
-  format.html 
-  format.js do 
+  def index
+    @emails = Email.all
+    respond_to do |format|
+      format.html 
+      format.js do 
+        @email = Email.find(params[:id])
+      end
+    end
+  end
+
+  def destroy
     @email = Email.find(params[:id])
+    @email.destroy
   end
-  end
- end
 end
 
 
